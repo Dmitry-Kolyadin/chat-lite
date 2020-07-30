78ab8bb5f4c8701933fc460f1062b8aaea0748ac
Dmitry-Kolyadin <kolyadin.2007@ya.ru>
2020-07-30 15:07:44 +0500

    snp-2_4_1

--- maim/login.py
+++ maim/login.py
@@ -5,30 +5,32 @@
 
 
 
+# вход с нового устройства
+def logining(data):
+    pass
 
+# вход с зарегистрированным устройством
+def login_devise(data):
+    pass
 
-
-
-
-
-
-
+# создание аккаунта
 def reg():
-    login=input("")
-    email=input("")
-    fist_name=input("")
-    last_name=input("")
-    promo_kod=input("")
-    password=input("")
-    last_password=input("")
-    kapcha=input("")
-    name_devais=input("Введите имя этого устройства")
+    login=input("Введите логин\n")
+    email=input("\n")
+    fist_name=input("\n")
+    last_name=input("\n")
+    promo_kod=input("\n")
+    password=input("Введите пароль\n")
+    last_password=input("Повторите пароль\n")
+    kapcha=input("\n")
+    name_devais=input("Введите имя этого устройства\n")
 
+# вход в аккаунт
 def login():
-    login=input("")
-    password=input("")
+    login=input("Введите ваш логин\n")
+    password=input("Введите ваш пароль\n")
     while 1:
-        new_devais_input=input("")
+        new_devais_input=input("Это новое устройство? (да / нет)\n")
         if new_devais_input=="Да" or new_devais_input=="да" or new_devais_input=="ДА" or new_devais_input=="дА":
             new_devais=1
             break
@@ -38,16 +40,40 @@ def login():
         else:
             print('Вы ввели некоректный ответ')
     if new_devais:
-        name_devais=input("Введите имя этого устройства")
+        name_devais=input("Введите имя этого устройства\n")
+        data={
+            'new_akkaynt':0,
+            'login':login,
+            'password':password,
+            'new_devais':new_devais,
+            'name_devais':name_devais,
+            }
+        print(data)
+        logining(data)
+
+    # вход с зарегистрированным устройсвом
     else:
-        pass
+        data={
+            'new_akkaynt':0,
+            'login':login,
+            'password':password,
+            'new_devais':new_devais,
+            }
+        print(data)
+        login_devise(data)
 
-
+#выбор регистрация/вход
 def akkyasorno():
-    akk=input("У вас есть аккаунт? (да / нет)")
-    if akk=="Да" or akk=="да" or akk=="ДА" or akk=="дА":
-        login()
-    elif akk=="нет" or akk=="Нет" or akk=="НЕТ" or akk=="нЕТ":
-        reg()
+    while 1:
+        akk=input("У вас есть аккаунт? (да / нет)\n")
+        if akk=="Да" or akk=="да" or akk=="ДА" or akk=="дА":
+            login()
+            break
+        elif akk=="нет" or akk=="Нет" or akk=="НЕТ" or akk=="нЕТ":
+            reg()
+            break
+        else:
+            print('Вы ввели некоректный ответ')
+
 
 akkyasorno()
