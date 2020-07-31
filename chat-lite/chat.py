8318730ddbd6cbac1a9ab66f4cd2dfc23130d254
Dmitry-Kolyadin <kolyadin.2007@ya.ru>
2020-07-31 12:35:17 +0500

    aaaaaaaaaaaaaa

--- chat-lite/chat.py
+++ chat-lite/chat.py
@@ -1,52 +1,80 @@
 import threading, random
 import requests
+import json
 
+shab = {
+    "httpMethod": "POST",
+    "headers": {
+        "Accept": "*/*",
+        "Content-Length": "13",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "curl/7.58.0",
+        "X-Real-Remote-Address": "[88.99.0.24]:37310",
+        "X-Request-Id": "cd0d12cd-c5f1-4348-9dff-c50a78f1eb79",
+        "X-Trace-Id": "92c5ad34-54f7-41df-a368-d4361bf376eb"
+    },
+    "path": "",
+    "multiValueHeaders": {
+        "Accept": [
+            "*/*"
+        ],
+        "Content-Length": [
+            "13"
+        ],
+        "Content-Type": [
+            "application/x-www-form-urlencoded"
+        ],
+        "User-Agent": [
+            "curl/7.58.0"
+        ],
+        "X-Real-Remote-Address": [
+            "[88.99.0.24]:37310"
+        ],
+        "X-Request-Id": [
+            "cd0d12cd-c5f1-4348-9dff-c50a78f1eb79"
+        ],
+        "X-Trace-Id": [
+            "92c5ad34-54f7-41df-a368-d4361bf376eb"
+        ]
+    },
+    "queryStringParameters": {
+        "a": "2",
+        "b": "1"
+    },
+    "multiValueQueryStringParameters": {
+        "a": [
+            "1",
+            "2"
+        ],
+        "b": [
+            "1"
+        ]
+    },
+    "requestContext": {
+        "identity": {
+            "sourceIp": "88.99.0.24",
+            "userAgent": "curl/7.58.0"
+        },
+        "httpMethod": "POST",
+        "requestId": "cd0d12cd-c5f1-4348-9dff-c50a78f1eb79",
+        "requestTime": "26/Dec/2019:14:22:07 +0000",
+        "requestTimeEpoch": 1577370127
+    },
+    "body": {"new_akkaynt": 0, "login": "login", "password": "pass", "new_devais": 1, "name_devais": "ffff"},
+    "isBase64Encoded": 1
+}
 
-data = {'QueueUrl':'data'}
+data = {"new_akkaynt": 0, "login": "login", "password": "pass", "new_devais": 1, "name_devais": "ffff"}
 
+#data=json.dumps(data)
+#shab['body']=data
 url = 'https://functions.yandexcloud.net/d4enqmko3pkmobp9n74m'
-
-r = requests.post(url, data = data)
+#print(data['new_akkaynt'])Content-Type: application/json
+print(type(shab))
+r = requests.post(url, data = shab, json=data)
 print(r)
 print(r.text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(type(r.text))
 
 
 
